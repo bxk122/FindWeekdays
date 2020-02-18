07268b95fba6a85e476606d13ab5eab8c058959e
Badal Karki <53354741+bxk122@users.noreply.github.com>
2020-02-17 20:42:45 -0500

    Create WeekDaysFind.java

---
+++ WeekDaysFind.java
@@ -0,0 +1,43 @@
+//This program is to find a day on the basis of given day and the given number.
+//For example: if given day is "Sunday" and the given number is 5
+//it should return "Friday".]
+
+import java.util.*;
+import java.util.Map.Entry;
+
+public class CogoLab
+{
+	public String TestDays(String S, long K) 
+	{
+        HashMap<Integer,String> days = new HashMap<Integer,String>();
+		int i = 0;
+		String[] weeks = new String[] {"Mon", "Tue", "Wed", "Thurs", "Fri" , "Sat", "Sun"};
+		for(i = 0; i < weeks.length; i++) 
+		{
+			days.put(i, weeks[i]);
+		}
+		
+		for (Entry<Integer, String> m:days.entrySet())
+		{ 
+			if(S.equals(m.getValue())) 
+			 {
+				//get key corresponding to days
+				 int z = m.getKey();
+				 //calculate the key of the given days based on given number( long k)
+				 //mod 7 gives the range of number between  0 to 7  
+				 long y = (z + K) % 7;
+				 //get the value(days) based on calculated key
+				 String val = days.get((int) y);
+				 System.out.println(val);
+			 }
+			 
+	    } 
+		return null;
+		
+	}
+	public static void main(String arg[]) 
+	{
+		CogoLab c = new CogoLab();
+		c.TestDays("Thurs",15236100);
+	}
+}
